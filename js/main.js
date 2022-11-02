d3f33ca997084f3d1d40c35d445defa0bec1e2a1
alessandro-cordoglio <alessandro.cord20@gmail.com>
2022-11-02 21:43:53 +0100

    add: on click event per la creazione dei box

---
+++ js/main.js
@@ -0,0 +1,31 @@
+"use strict"
+/* VARIABLES */
+/* variabili di supporto */
+const board= document.querySelector("#board")
+const start= document.getElementById("start")
+/* FUNCTIONS */
+//funzione x creazione dei box
+function createBox(cellnumber){
+    for (let i = 1; i <= cellnumber; i++) {
+        const box= document.createElement("div")
+        box.innerHTML= i
+        box.classList.add("box")
+        board.append(box)
+    }
+}
+
+/* MAIN SCRIPT */
+start.addEventListener("click", function(){
+    const difficultyChoice= document.getElementById("difChoice").value
+    document.querySelector("#start").disabled = true;
+    if (difficultyChoice==="hard") {
+        createBox(100)
+    }else if(difficultyChoice==="normal"){
+        createBox(81)
+        box.classList.replace("box","box_normal");
+    }else if(difficultyChoice==="easy"){
+        createBox(49)
+        box.classList.replace("box","box_easy");
+    }
+});  
+
